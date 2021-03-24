0c7a4af4039c53635370f181cc2d27ea6c6ffe8d
PatrykWalach <patryk_walach_github@outlook.com>
2021-03-24 21:32:14 +0100

    feat: submit 2

--- 2.cpp
+++ 2.cpp
@@ -11,17 +11,7 @@
 #undef SHRT_MAX
 #define SHRT_MAX 0x7fff
 
-bool fn(std::vector<std::vector<short>>& edges, std::vector<bool>& taken,
-        short index) {
-  for (auto& edge : edges[index]) {
-    if (taken[edge] && !fn(edges, taken, edge)) {
-      continue;
-    }
-    taken[edge] = true;
-    return true;
-  }
-  return false;
-}
+bool isBlack(short i) { return i % 2 == 0; }
 
 int main() {
   std::ios_base::sync_with_stdio(false);
@@ -31,38 +21,101 @@ int main() {
   short n;
   std::cin >> n;
 
-  std::vector<std::vector<short>> board(n, std::vector<short>(n));
-  std::vector<std::vector<std::vector<std::tuple<short, short>>>> edges(
-      n, std::vector<std::vector<std::tuple<short, short>>>(n));
+  std::vector<short> vertecies(n * n);
 
-  for (auto i = 0; i < n; i++) {
-    for (auto j = 0; j < n; j++) {
-      std::cin >> board[i][j];
+  std::vector<std::vector<short>> edges(n * n);
+
+  for (auto i = 0; i < n * n; i++) {
+    std::cin >> vertecies[i];
+  }
+
+  for (auto k = 0; k < n * n; k++) {
+    // for (auto j = 0; j < n; j++) {
+    if (vertecies[k] == 0) {
+      continue;
+    }
+
+    auto y1 = (((vertecies[k] / 2 + 1) % 2) * 2 + 1) * ((k % 2) * -2 + 1);
+
+    if ((k + y1) / n == k / n && vertecies[k + y1] != 0) {
+      edges[k].push_back(k + y1);
+    }
+
+    auto y2 = (vertecies[k] % 2) * 2 * ((k % 2) * -2 + 1);
+
+    if ((k + y2) / n != k / n) {
+      continue;
+    }
+
+    if (k + n < n * n && vertecies[k + n + y2] != 0) {
+      edges[k].push_back(k + n + y2);
+    }
+
+    if (k - n >= 0 && vertecies[k - n + y2] != 0) {
+      edges[k].push_back(k - n + y2);
     }
   }
 
-  for (auto i = 0; i < n; i++) {
-    for (auto j = 0; j < n; j++) {
-      if (board[i][j] == 0) {
+  std::vector<short> pairs(n * n, -1);
+  std::vector<short> branches(n * n);
+
+  for (auto i = 0; i < n * n; i += 2) {
+    if (pairs[i] > -1) {
+      continue;
+    }
+
+    std::vector<short> visited(n * n, false);
+    std::queue<short> queue;
+
+    visited[i] = true;
+    branches[i] = -1;
+    queue.push(i);
+
+    while (!queue.empty()) {
+      auto j = queue.front();
+      queue.pop();
+
+      if (!isBlack(j)) {
+        if (pairs[j] <= -1) {
+          while (branches[j] > -1) {
+            if (!isBlack(j)) {
+              pairs[j] = branches[j];
+              pairs[branches[j]] = j;
+            }
+            j = branches[j];
+          }
+          break;
+        }
+        pairs[branches[j]] = j;
+        visited[branches[j]] = true;
+        queue.push(pairs[j]);
         continue;
       }
-      if (board[i][j] == 1) {
-        edges[i][j].push_back(std::make_tuple(i - 1, j + 2));
-        edges[i][j].push_back(std::make_tuple(i, j + 3));
-        edges[i][j].push_back(std::make_tuple(i + 1, j + 2));
-        continue;
+
+      for (auto& edge : edges[j]) {
+        if (visited[edge]) {
+          continue;
+        }
+        visited[edge] = true;
+        branches[edge] = j;
+        queue.push(edge);
       }
-      if (board[i][j] == 2) {
-        edges[i][j].push_back(std::make_tuple(i - 1, j));
-        edges[i][j].push_back(std::make_tuple(i, j + 1));
-        edges[i][j].push_back(std::make_tuple(i + 1, j));
-      }
-      edges[i][j].push_back(std::make_tuple(i - 1, j + 2));
-      edges[i][j].push_back(std::make_tuple(i, j + 1));
-      edges[i][j].push_back(std::make_tuple(i + 1, j + 2));
     }
   }
 
-  //   std::cout <<n*n - r;
-  //   std::cout << 19;
+  int r = 0;
+  for (int i = 0; i < n * n; i++) {
+    // std::cout << i << "->" << pairs[i] << std::endl;
+
+    if (pairs[i] > -1) {
+      r++;
+    }
+  }
+
+  // std::cout << r;
+  // r - zbitych
+  // znaleźć max par bijących
+  // jedna z dwóch w parze, może się zbić
+
+  std::cout << n * n - r / 2;
 }
